66134e1075d95417970b41407e225ec8512e38d2
rochuchu <rpenalva@alumno.huergo.edu.ar>
2026-05-16 04:31:15 -0300

    comentarios

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main():
     clinica = ClinicaVeterinaria("Veterinaria Lorde 81")
     dao = ClinicaDAO()
 
+    # El sistema valida que no haya datos inconsistentes al cargar.
     try:
         dao.cargar(clinica, "data/")
+    # Si algún archivo falta la primera vez, comenzaremos con datos vacíos
     except FileNotFoundError:
         pass
+    # Si hay un archivo JSON mal formado, comunicamos al usuario
     except json.JSONDecodeError as e:
         print("Error: un archivo en data/ no es JSON válido.", e)
+    # Hay datos inválidos según la lógica (duplicado, incompleto, superpuesto)
     except (ValueError, KeyError) as e:
         print("Error al cargar (repetido, incompleto, turno inválido):", e)
 
